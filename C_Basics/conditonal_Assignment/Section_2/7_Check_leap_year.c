a41a89a06ece2bc571a961795141be1c034b2126
S.HEMANTH <hemanth.selam@gmail.com>
2023-06-19 14:37:26 +0530

    Conditional_statements_and_other_assignments

--- C_Basics/conditonal_Assignment/Section_2/7_Check_leap_year.c
+++ C_Basics/conditonal_Assignment/Section_2/7_Check_leap_year.c
@@ -7,17 +7,23 @@ if (year % 400 == 0)
 {
 printf("%d is a leap year.", year);
 }
-else if (year % 100 == 0)
+else
+{
+if (year % 100 == 0)
 {
 printf("%d is not a leap year.", year);
 }
-else if (year % 4 == 0)
+else 
+{
+if (year % 4 == 0)
 {
 printf("%d is a leap year.", year);
 }
 else
 {
 printf("%d is not a leap year.", year);
+}
+}
 }
 return 0;
 }
