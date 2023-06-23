c73f1259d65ec6185d00122fcf424606cff4de3d
S.HEMANTH <hemanth.selam@gmail.com>
2023-06-23 11:22:03 +0530

    logical_operators_assignment

--- C_Basics/logical_operator/7_leap_year_or_not.c
+++ C_Basics/logical_operator/7_leap_year_or_not.c
@@ -3,9 +3,9 @@ int main() {
 int year;
 printf("Enter a year: ");
 scanf("%d", &year);
-if (year % 400 == 0 || year % 4 == 0) 
+if (year % 100 != 0 || year % 4 == 0) 
 printf("%d is a leap year.\n", year);
-else if (year % 100 == 0)
+else if (year % 400 == 0)
 printf("%d is not a leap year.\n", year);
 else
 printf("%d is not a leap year.\n", year);
