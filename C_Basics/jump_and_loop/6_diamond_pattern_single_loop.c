dd3f0b617db92c8bebfa68b253247f7140df984d
S.HEMANTH <hemanth.selam@gmail.com>
2023-07-12 08:09:00 +0530

    jump_and_nested_loop_assignment

--- C_Basics/jump_and_loop/6_diamond_pattern_single_loop.c
+++ C_Basics/jump_and_loop/6_diamond_pattern_single_loop.c
@@ -1,29 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int n, i, j, k;
+	int n, i=1, k=0;
 	printf("Enter the number: ");
 	scanf("%d",&n);
-	for(i = 1; i <= n; i++)
+	while(i <= n)
 	{
-		j=i;
-LBL1:
-
-		if(j<n)
-		{
-			printf(" ");
-			j++;
-			goto LBL1;		
-		}
-
-		k=1;
-LBL2:
-		if(k<(i*2))
+		if(k<i)
 		{
 			printf("*");
 			k++;
-			goto LBL2;
 		}
-		printf("\n");
+		else
+		{
+			printf(" ");
+			k=0;
+			i++;
+			printf("\n");
+		}
 	}
 }
