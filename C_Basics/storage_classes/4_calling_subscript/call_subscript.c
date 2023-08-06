82c1c22839d4ce4e1afb4aa4b8771d2377acfe3e
S.HEMANTH <hemanth.selam@gmail.com>
2023-08-06 22:47:21 +0530

    stotage_classes_assignment

--- C_Basics/storage_classes/4_calling_subscript/call_subscript.c
+++ C_Basics/storage_classes/4_calling_subscript/call_subscript.c
@@ -18,6 +18,8 @@ int main()
 void call(void)
 {
 	i++;
+	if(i==n)
+	{
 		if((n<10||n>20) && n%10==1)
 			printf("Hello! I am called %dst time\n",n);
 		else if((n<10||n>20) && n%10==2)
@@ -27,7 +29,7 @@ void call(void)
 		else if(n%10==0 || n%10>=4 || n/10==1)
 			printf("Hello! I am called %dth time\n",n);
 	}
-else if((i<10||i>20) && i%10==1)
+	else if((i<10||i>20) && i%10==1)
 		printf("I am called %dst time\n",i);
 	else if((i<10||i>20) && i%10==2)
 		printf("I am called %dnd time\n",i);
