01d7fbe4be642ff70cc0ed5f5a0d09d3619c1baf
S.HEMANTH <hemanth.selam@gmail.com>
2023-08-21 11:19:53 +0530

    strings assignment

--- C_Basics/strings/1_count_words.c
+++ C_Basics/strings/1_count_words.c
@@ -5,13 +5,16 @@
 int main()
 {
 	int c=0;
-	char str[50];
+	char str[100];
 	printf("Enter a sentance:\n");
 	scanf("%[^\n]s",str);
-	for(int i=0;str[i];i++)
+	for(int i=0;str[i]!='\0';i++)
 	{
-		if(str[i]==' ' || str[i]==',' || str[i]=='!' || str[i]=='?' || str[i]=='.')
-			c++;
+		if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z') || (str[i]>='0' && str[i]<='9'))
+		{
+			 if(str[i+1]<'0' || (str[i+1]>'9' && str[i+1]<'A') || (str[i+1]>'Z' && str[i+1]<'a') || str[i+1]>'z')
+				c++;
+		}	
 	}
-	printf("Number of word = %d\n",c);
+	printf("\nNumber of word = %d\n",c);
 }
