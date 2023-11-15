c0f5afdf1c588a3059c2fe776646865d5f6c3f92
S.HEMANTH <hemanth.selam@gmail.com>
2023-11-16 00:11:59 +0530

    process management

---
+++ LSP/process_management/mini_project/1.my_shell.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include<unistd.h>
+
+void shell(char command[])
+{
+
+	execl("/bin/sh", "sh", "-c",command,NULL);
+	exit(0);
+}
+
+int main()
+{
+	char *command[1024];
+	pid_t pid;
+	while(1)
+	{
+		printf("&:");
+		scanf("%[^\n]s",command);
+		getchar();	
+		if(!(strcmp(command,"exit")))
+		{
+			return 0;
+		}	
+		pid=fork();
+		if(pid==0)
+		{
+			shell(command);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+}
+
