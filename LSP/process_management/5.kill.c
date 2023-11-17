75cba3ed83a55d5b7bb5f205cf096ef8e3fca246
S.HEMANTH <hemanth.selam@gmail.com>
2023-11-17 22:33:55 +0530

    Process management

--- LSP/process_management/5.kill.c
+++ LSP/process_management/5.kill.c
@@ -5,13 +5,21 @@
 
 int main(int argc,char *argv[])
 {
-if(argc<2)
-printf("Invalid Arguments\n");
-else
-{
-pid_t pid;
-pid=atoi(argv[1]);
-kill(pid,9);
-}
-return 0;
+printf("%d\n",argc);
+	if(argc<2)
+		printf("Invalid Arguments\n");
+	else if(argc == 2)
+	{
+		pid_t pid;
+		pid=atoi(argv[1]);
+		kill(pid,9);
+	}
+	else if(argc ==3)
+	{
+		pid_t pid,sig;
+		pid=atoi(argv[2]);
+		sig=atoi(argv[1]);
+		kill(pid,sig);
+	}
+	return 0;
 }
